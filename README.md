ef1b949e264e9fe8cbf3d1c12bf3142a91279299
Bruno Vieira <vieira.es@gmail.com>
2022-04-15 03:59:17 -0300

    docs/added documentation API

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 Api como uma solução de monitoria de tempos de apresentação, voltada para congressos e eventos com palestras simultâneas que necessitam de um sistema que acompanhe e monitore o tempo das apresentações nas salas de audiovisual, trazendo assim um acompanhamento do tempo de cada sala com apresentações. 
 
 Consulte através de um gerenciador central único os tempos das apresentações.
+
+## Link da API
+https://documenter.getpostman.com/view/20418848/Uyr4LfjN
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const crypto = require('crypto');
+const { request } = require('http');
 
 
 
@@ -72,11 +73,12 @@ app.get("/presenter", (request, response) => {
 
 app.post("/register-station", (request, response) => {
 
-    const { stationAddress, isActive } = request.body;
+    const { stationAddress, stationName, isActive } = request.body;
 
     const objeto = {
         stationId: uuidv4(),
         stationAddress: stationAddress,
+        stationName: stationName,
         created_at: new Date(),
         isActive: isActive,
         lectures: []
@@ -84,6 +86,7 @@ app.post("/register-station", (request, response) => {
 
     stationsDB.push({
         stationId: objeto.stationId,
+        stationName: objeto.stationName,
         stationAddress: objeto.stationAddress,
         created_at: objeto.created_at,
         isActive: objeto.isActive,
@@ -95,6 +98,7 @@ app.post("/register-station", (request, response) => {
     return response.status(200).json({
         message: "station created",
         stationId: objeto.stationId,
+        stationName: objeto.stationName,
         stationAddress: objeto.stationAddress,
         created_at: objeto.created_at,
         isActive: objeto.isActive,
@@ -106,34 +110,147 @@ app.post("/register-station", (request, response) => {
 
 
 
-function IsStationActive(stationId) {
+// TODO: corrigir esse middleware
+function verifyStationStatus(request, response, next) {
 
-    const station = stationsDB.find(station => station.stationId === stationId);
 
-    if (station.isActive === true) {
-        return true;
-    } else {
-        return false;
+    const parametroDoRequest = request.params.isActive;
+
+    const VarclassificaArray = function classificaArray(algo) {
+
+        if (parametroDoRequest === true) {
+            const arrayResultados = stationsDB.filter(station => station.isActive === true);
+            return array = arrayResultados;
+
+
+
+
+        } else if (parametroDoRequest === false) {
+            const arrayResultados = stationsDB.filter(station => station.isActive === false);
+            return array = arrayResultados;
+
+
+
+        } else {
+            return array = null;
+
+        }
+
+    };
+    return VarclassificaArray
+
+
+    const arrayGeral = function retorno(arrayResultados) {
+
+
+        const calculosInternos = function () {
+            if (parametroDoRequest === true) {
+                arrayResultados = stationsDB.filter(station => station.isActive === true);
+                response = arrayResultados;
+                return next(arrayResultados);
+
+
+            } else if (parametroDoRequest === false) {
+                arrayResultados = stationsDB.filter(station => station.isActive === false);
+                response = arrayResultados;
+                return next(arrayResultados);
+
+
+            } else {
+                response = arrayResultados;
+                return next(arrayResultados = null);
+
+            }
+
+        }
+    };
+    retorno(parametroDoRequest);
+
+
+
+
+
+
+    return arrayGeral;
+
+};
+
+
+
+// const stationStatusTrue = stationsDB.filter(station => station.isActive === true);
+// const stationStatusFalse = stationsDB.filter(station => station.isActive === false);
+
+
+/* 
+    if (stationStatusTrue.length > 0) {
+ 
+        return response.status(200).json({
+            message: "active stations",
+            stationName: stationStatusTrue.stationName,
+            stationAddress: stationStatusTrue.stationAddress,
+            isActive: stationStatusTrue.isActive,
+ 
+        });
+    } else if (stationStatusFalse.length > 0) {
+        return response.status(200).json({
+            message: "inactive stations",
+            stationName: stationStatusFalse.stationName,
+            stationAddress: stationStatusFalse.stationAddress,
+            isActive: stationStatusFalse.isActive,
+ 
+        });
     }
-}
+ 
+    return next();
+ */
+
+// como fazer uma variavel dentro de uma functions ficar exposta para outras functions?
+//resposta: usar o module.exports
+
+// como uar o module.exports dentro de uma function?
+//resposta: usar o return
+
+
+// exemplo de um module.exports dentro de uma function
+// module.exports = function() {
+//     return {
+//         message: "hello world"
+//     }
+
+
+// response = stationsFilterTrue.length > 0 ? response.status(200).json({
+//     message: "stations active found",
+//     stations: stationsFilterTrue
+// }) : response.status(404).json({
+
+//     message: "no stations active found",
+// });
+
+
+
+
+/* 
+
+app.get('/datas', async (req: Request, res: Response)) => {
+    const datas = await Accidents()
+    return res.json({datas})
+
+*/
+
+//TODO: corrigir esse middleware
+app.get("/stations/:isActive", verifyStationStatus, (request, response) => {
 
 
 
 
 
-app.get("/stations/:isActive", IsStationActive,(request, response) => {
+    //    const { isActive } = request.params;
 
-    const { station } = request;
-    const { isActive } = request.params;
-
-
-
-    const objeto = stationsDB.map(station => {
+    const objeto = arrayResultados.map(station => {
         return {
             stationId: station.stationId,
             stationAddress: station.stationAddress,
             isActive: station.isActive
-
         };
 
     });
@@ -144,9 +261,6 @@ app.get("/stations/:isActive", IsStationActive,(request, response) => {
         isActive: objeto.isActive
     });
 });
-//final
-
-
 
 
 //uma Lecture poderia ser criada sem um presenter?
